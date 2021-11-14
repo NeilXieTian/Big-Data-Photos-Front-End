482c1c52d88653fddec28e72bb8a8ca4bb46bdcf
NeilXie <neilxietian@gmail.com>
2021-11-13 23:47:11 -0500

    inital commit

---
+++ index_backup.js
@@ -0,0 +1,162 @@
+var name = '';
+var file = null;
+var encoded = null;
+var fileExt = null;
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+const synth = window.speechSynthesis;
+const recognition = new SpeechRecognition();
+const icon = document.querySelector('i.fa.fa-microphone')
+
+function previewFile(input) {
+  var reader = new FileReader();
+  file = input.files[0];
+  name = file.name;
+  fileExt = name.split(".")[1];
+  reader.onload = function (e) {
+    var src = e.target.result;
+    var newImage = document.createElement("img");
+    newImage.src = src;
+    encoded = newImage.outerHTML;
+  }
+  reader.readAsDataURL(file);
+}
+
+function upload() {
+  last_index_quote = encoded.lastIndexOf('"');
+  if (fileExt == 'jpg' || fileExt == 'jpeg') {
+    encodedStr = encoded.substring(33, last_index_quote);
+  }
+  else {
+    encodedStr = encoded.substring(32, last_index_quote);
+  }
+
+  var apigClient = apigClientFactory.newClient({ apiKey: "jNMoDcJqtf5F5B48cs4efaz21rmfYCRD3xJwt0ss" });
+  var labels = document.getElementById("custom_labels").value;
+  var params = {
+    "filename": name,
+    "bucket": "hw2photos",
+    "Content-Type": file.type+";base64",
+//     "Content-Type": file.type,
+    "x-amz-meta-customLabels": labels,
+    "x-api-key": "jNMoDcJqtf5F5B48cs4efaz21rmfYCRD3xJwt0ss",
+  };
+
+  var additionalParams = {
+    headers: {
+      "Content-Type": file.type+";base64",
+//       "Content-Type": file.type,
+      "x-amz-meta-customLabels": labels,
+    }
+  };
+
+  apigClient.photosFilenamePut(params, encodedStr, additionalParams)
+    .then(function (result) {
+      console.log('success OK');
+      console.log(result);
+      alert("Photo uploaded successfully!");
+    }).catch(function (result) {
+      console.log(result);
+    });
+  
+}
+
+function searchFromVoice() {
+  recognition.start();
+  recognition.onresult = (event) => {
+    var speechToText = event.results[0][0].transcript;
+    document.getElementById("search").value += speechToText;
+    console.log(speechToText)
+
+    var apigClient = apigClientFactory.newClient({ apiKey: "jNMoDcJqtf5F5B48cs4efaz21rmfYCRD3xJwt0ss" });
+    var params = {
+      "q": speechToText
+    };
+    var body = {
+      "q": speechToText
+    };
+
+    var additionalParams = {
+      queryParams: {
+        q: speechToText
+      }
+    };
+
+    apigClient.searchGet(params, body, additionalParams)
+      .then(function (result) {
+        console.log('success OK');
+        showImages(result.data.results);
+        console.log(result.data.results);
+      }).catch(function (result) {
+        console.log(result);
+        console.log(speechToText);
+      });
+
+    // console.log(speechToText);
+  }//
+}
+
+
+function search() {
+  var searchTerm = document.getElementById("search").value;
+  var apigClient = apigClientFactory.newClient({ apiKey: "jNMoDcJqtf5F5B48cs4efaz21rmfYCRD3xJwt0ss" });
+  var params = {
+    "q": searchTerm,
+    // "x-api-key": "jNMoDcJqtf5F5B48cs4efaz21rmfYCRD3xJwt0ss",
+  };
+  var body = {
+    "q": searchTerm
+  };
+
+  var additionalParams = {
+    queryParams: {
+      q: searchTerm
+    }
+  };
+  console.log(searchTerm);
+  apigClient.searchGet({q:searchTerm},body,additionalParams)
+    .then(function (result) {
+      console.log('success OK');
+      console.log(result)
+      showImages(result.data);
+    }).catch(function (result) {
+      console.log(result);
+    });
+}
+
+function showImages(res) {
+  var newDiv = document.getElementById("div");
+  if(typeof(newDiv) != 'undefined' && newDiv != null){
+  while (newDiv.firstChild) {
+    newDiv.removeChild(newDiv.firstChild);
+  }
+}
+  console.log(res)
+  console.log(res.results)
+
+  if (res.results.length == 0) {
+    var newContent = document.createTextNode("No image to display");
+    newDiv.appendChild(newContent);
+    var currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+  else {
+    for (var i = 0; i < res.results.length; i++) {
+
+      var newDiv = document.getElementById("div");
+      newDiv.style.display = 'inline'
+      //var newContent = document.createElement("img");
+      var newContent = new Image();
+//       newContent.src = "https://s3.amazonaws.com/"+res.results[i]['bucket']+"/"+res[i]['objectKey'];
+      newContent.src = res.results[i];
+      newContent.style.padding = "20px";
+      newContent.style.height = "200px";
+      newContent.style.width = "200px";
+      newDiv.appendChild(newContent);
+      var currentDiv = document.getElementById("div1");
+      document.body.insertBefore(newDiv, currentDiv);
+    }
+  }
+}
+
+
+
